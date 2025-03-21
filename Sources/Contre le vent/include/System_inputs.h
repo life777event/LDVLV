cf8af48f3fe6fbacc979d8a1293df33f9453bbc3
anarticho <ptrkrptkne@gmail.com>
2025-03-21 15:36:08 -0700

    Changes

--- Sources/Contre le vent/include/System_inputs.h
+++ Sources/Contre le vent/include/System_inputs.h
@@ -15,45 +15,73 @@ namespace System
 		};
 
 		/// Constructor, with window.
-		Inputs();
+		explicit Inputs(sf::WindowBase& win0);
 
 		/// Add listener.
-		void add_rx(std::vector<typename C> ins, Irx* irx);
+		typedef sf::Mouse::Button Mouse_event;
+		typedef sf::Keyboard::Key Keyboard_event;
+		void add_mouse_list(std::vector<Mouse_event> ins, Irx* irx);
+		void add_keyboard_list(std::vector<Keyboard_event> ins, Irx* irx);
 
 		/// Step to get input from window and forward to relevant listeners.
-		void step(C in);
+		void step();
 
 	private:
-		sf::WindowBase& win;			///< Window, to poll events.
-		std::map<typename C, Irx*> map;	///< Map of keys/listener.
+		sf::WindowBase& win;								///< Window, to poll events.
+
+		typedef std::map<Mouse_event, Irx*> Mouse_listener;
+		typedef std::map<Keyboard_event, Irx*> Keyboard_listener;
+		Mouse_listener		mouse_list;		///< Map of listener for mouse.
+		Keyboard_listener	keyboard_list;	///< Map of listener for mouse.
+
+		template <typename IN>
+		static void add_list(std::vector<IN> ins, Irx* irx, std::map<IN, Irx*>& map);
 
 		Inputs(const Inputs& orig); ///< = delete
 		Inputs& operator=(const Inputs& orig); ///< = delete
 	};
 
-	template <typename C>
-	Inputs<C>::Inputs()
+	template <typename IN>
+	void Inputs::add_list<IN>(std::vector<IN> ins, Irx* irx, std::map<IN, Irx*>& map)
 	{
-	};
-
-	template <typename C>
-	void Inputs<C>::add_rx(std::vector<typename C> ins, Irx* irx)
-	{
-		for(C in : ins)
+		for(IN in : ins)
 		{
-			map.insert(in, irx);
+			map.insert({ in, irx });
 		}
+	}
+
+	inline Inputs::Inputs(sf::WindowBase& win0) :
+		win(win0)
+	{
 	};
 
-	template <typename C>
-	void Inputs<C>::step(C in)
+	inline void Inputs::add_mouse_list(std::vector<sf::Mouse::Button> ins, Irx* irx)
 	{
-		if(map.count(in))
+		add_list(ins, irx, mouse_list);
+	};
+
+	inline void Inputs::add_keyboard_list(std::vector<Keyboard_event> ins, Irx* irx)
+	{
+		add_list(ins, irx, keyboard_list);
+	};
+
+	inline void Inputs::step()
+	{
+		sf::Event tmp;
+		if(win.pollEvent(tmp))
 		{
-			for (Irx* irx : map.at(in))
+			if((tmp.type == sf::Event::MouseButtonPressed) && mouse_list.count(static_cast<sf::Event::KeyEvent>(tmp.key)))
 			{
-				irx->on_rx();
+
+			}
+			else if(tmp.type == sf::Event::KeyPressed)
+			{
+
+			}
+			else
+			{
+
 			}
 		}
-	};
+	}
 };
